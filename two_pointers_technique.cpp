950a0adad70860a32530f2545afae323449f462b
Jashan Bansal <jashanbansal7856@yahoo.com>
2020-10-16 18:11:03 +0530

    two pointers video added

---
+++ two_pointers_technique.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+// Link to the video: https://youtu.be/Ht0kdbFEfGE
+int main(){
+	Two pointer approach when:
+	1. Subarray problems: Either smallest subarray or longest subarray
+	2. Monotonic function: Either non-increasing or non-decreasing
+
+	Q1. Given an array of n positive integers. Find the length smallest subarray with sum greater than equal to given k. 
+
+	Q2. Given a string, find the length of the longest substring that contains at most k distinct characters. 
+
+	Q3. Given an array of size of size N. Find the length of longest subarray such that there exist maximum k equal pairs. 
+}
